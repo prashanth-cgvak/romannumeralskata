cd3e0b886ffd6a3a133bdb3cd6f6ea5ba0ab648b
Chris Emerson <chris.emerson@cpwonlinesolutions.com>
2015-04-23 12:34:02 +0100

    Initialise behat and provide some skeleton code

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,38 @@
+<?php
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use PHPUnit_Framework_Assert as Assert;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    private $input;
+    private $output;
+
+    /**
+     * @Given I have an input of :input
+     */
+    public function iHaveAnInputOf($input)
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * @When I run the code to convert it to a Roman Numeral
+     */
+    public function iRunTheCodeToConvertItToARomanNumeral()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should be given a return value of :expectedOutput
+     */
+    public function iShouldBeGivenAReturnValueOf($expectedOutput)
+    {
+        Assert::assertEquals($expectedOutput, $this->output);
+    }
+}
